5e06a39d1adee439d57a8262d67fdc4ddcf53373
luwenhai99999 <834299862@qq.com>
2023-11-22 13:23:05 +0800

    feat: create versionrc.cjs

---
+++ .versionrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  "header": "react-template",
+  "types": [
+    {"type": "feat", "section": "⭐ Features"},
+    {"type": "fix", "section": "🐞 Bug Fixes"},
+    {"type": "chore", "hidden": true},
+    {"type": "docs", "hidden": true},
+    {"type": "style", "hidden": true},
+    {"type": "refactor", "hidden": true},
+    {"type": "perf", "hidden": true},
+    {"type": "test", "hidden": true},
+    {"type": "build", "hidden": true},
+    {"type": "ci", "hidden": true},
+    {"type": "revert", "hidden": true},
+    {"type": "types", "hidden": true},
+  ]
+}
